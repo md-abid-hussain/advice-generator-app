73e06230fa20206d5728d739c788f8447bf71f88
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-10-19 21:23:10 +0530

    added colors theme is tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-cyan": "#CEE3E9",
+        "neon-green": "#52FFA8",
+        "grayish-blue": "#4E5D73",
+        "dark-grayish-blue": "#323A49",
+        "dark-blue": "#1F2632"
+      },
+      fontSize: {
+        "quote-font": "28px"
+      }
+    },
   },
   plugins: [],
 }
